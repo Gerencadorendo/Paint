39f02f6b14152f867f53e24422f86534ed3742a0
Gerencadorendo <121314300+Gerencadorendo@users.noreply.github.com>
2023-07-25 18:04:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+ var mouseEvent = "empty";
+    var lastPositionX, lastPositionY;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "black";
+    widthLine = 1;
+
+    canvas.addEventListener("mousedown", myMouseDown);
+    function myMouseDown(e)
+    {
+         mouseEvent = "mouseDown";
+    }
+
+    canvas.addEventListener("mouseup", myMouseUp);
+    function myMouseUp(e)
+    {
+         mouseEvent = "mouseUp";
+    }
+
+    canvas.addEventListener("mousemove", myMouseMove);
+    function myMouseMove(e)
+    {
+        PositionMouseX = e.clientX - canvas.offsetLeft
+        PositionMouseY = e.clientY - canvas.offsetTop
+
+        if (mouseEvent == "mouseDown") {
+            ctx.beginPath();
+            ctx.strokeStyle = color;
+            ctx.lineWidth = widthLine;
+
+            ctx.moveTo(lastPositionX, lastPositionY);
+
+            ctx.lineTo(PositionMouseX, PositionMouseY);
+            ctx.stroke();
+        }
+
+        lastPositionX = PositionMouseX;
+        lastPositionY = PositionMouseY;
+    }
